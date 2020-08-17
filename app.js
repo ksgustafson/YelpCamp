e67e47bf6e6d4eccb57f0ba936f52097b0161816
Hat Brackets <hat.brackets@gmail.com>
2020-08-17 14:59:23 -0500

    styling of campground info pages with custom css

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/public`));
 
 // seed database with campgrounds and comments
 seedDB();
